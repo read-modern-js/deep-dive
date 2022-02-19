8627734320530f853f485348175e1101bdf58745
yanggak12 <yanggak12@naver.com>
2022-02-19 15:14:01 +0900

    R7 : array from callback

--- Round_7/arrayFrom.js
+++ Round_7/arrayFrom.js
@@ -1,8 +1,8 @@
 // 예제 27-29
 // 503페이지
 
-const wrongExample = Array.from({ length: 3 }, (i) => i);
+const Example = Array.from({ length: 3, 0: 1, 1: 2, 2: 3 }, (v, i) => v); // map 함수가 콜백함수로!
 const arr = Array.from({ length: 3 }, (_, i) => i);
 
-console.log(wrongExample); // [undefined, undefined, undefined]
+console.log(Example); // [1, 2, 3]
 console.log(arr); // [0, 1, 2]
